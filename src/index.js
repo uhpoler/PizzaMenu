319b1aa36e2c82a6d1f81b4cc94be5b370e60921
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-10 15:33:30 +0300

    Added conditional rendering with multiple returns

--- src/index.js
+++ src/index.js
@@ -107,6 +107,8 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.name} />
