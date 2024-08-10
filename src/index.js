81c9d060bf448fd69037c0de5b3d96bb3e166c61
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-10 13:09:27 +0300

    Created more components

--- src/index.js
+++ src/index.js
@@ -49,11 +49,31 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1> Hello React! </h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h1>Our menu</h1>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} 6We are currently open!"</footer>
   );
 }
 
